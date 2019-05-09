f787dd983dcfb89dcebe4bcffd9a4cecfd15a731
tiefengeist <sahirnambiar@gmail.com>
2019-05-09 11:10:31 -0700

    test

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+    @user = User.find_by(username: user_params[:username])
+    if @user && @user.authenticate(user_params[:password])
+
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: token }, status: :accepted
+    else
+      render json: { message: 'Invalid username or password'}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
